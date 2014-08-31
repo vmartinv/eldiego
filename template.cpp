e5ed542fcd87c39a5e27d044bf223ec81e33b026
mvpossum <mvillagra0@gmail.com>
2014-08-31 17:49:20 -0300

    v1

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+#include <string>
+using namespace std;
+#define dprint(v) cerr << #v"=" << v << endl //;)
+#define forr(i,a,b) for(int i=(a); i<(b); i++)
+#define forn(i,n) forr(i,0,n)
+#define dforsn(i,a,b) for(int i=(b)-1; i>=a; i--)
+#define forall(it,v) for(typeof(v.begin()) it=v.begin();it!=v.end();++it)
+#define sz(c) ((int)c.size())
+#define zero(v) memset(v, 0, sizeof(v))
+typedef long long ll;
+typedef pair<int,int> ii;
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.in", "r", stdin);
+#endif
+	while(){
+	
+	}
+    return 0;
+}
+
