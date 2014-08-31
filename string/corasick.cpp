45fb47a81e110d802d8add0b94b8202025181f28
mvpossum <mvillagra0@gmail.com>
2014-08-31 20:58:39 -0300

    corasick

--- string/corasick.cpp
+++ string/corasick.cpp
@@ -6,28 +6,26 @@ using namespace std;
 #define forn(i,n) for(int i=0; i<n; i++)
 #define sz(c) ((int)c.size())
 
-
 struct trie{
 	map<char, trie> next;
 	trie* tran[256];
 	int idhoja, szhoja;//id de la hoja o 0 si no lo es
-	trie *padre;
-	trie *link//lleva al sufijo mas largo
-	trie *match;//lleva a la hoja mas larga que es sufijo
+	//link lleva al sufijo mas largo, nxthoja lleva al mas largo pero que es hoja
+	trie *padre, *link, *nxthoja;
 	char pch;//caracter que conecta con padre
-	void add(const string &s, int id=1, int p=0){
+	void insert(const string &s, int id=1, int p=0){//id>0!!!
 		if(s[p]){
 			trie &ch=next[s[p]];
 			tran[(int)s[p]]=&ch;
 			ch.padre=this, ch.pch=s[p];
-			ch.add(s, id, p+1);
+			ch.insert(s, id, p+1);
 		}
 		else idhoja=id, szhoja=sz(s);
 	}
 	trie* get_link() {
 		if(!link){
-			if(!padre) link=this;
-			else if(!padre->padre) link=padre;
+			if(!padre) link=this;//es la raiz
+			else if(!padre->padre) link=padre;//hijo de la raiz
 			else link=padre->get_link()->get_tran(pch);
 		}
 		return link;
@@ -37,29 +35,27 @@ struct trie{
 			tran[c] = !padre? this : this->get_link()->get_tran(c);
 		return tran[c];
 	}
-	trie *get_match(){
-		if(!match)
-			match = get_link()->idhoja? link : link->match;
-		return match;
+	trie *get_nxthoja(){
+		if(!nxthoja) nxthoja = get_link()->idhoja? link : link->nxthoja;
+		return nxthoja;
 	}
-	void printocur(int p){
-		if(idhoja){
-			 cout << "found " << idhoja << "  at position " << p-szhoja+1 << endl;
-		}
-		if(get_match()) match->printocur(p);
+	void print(int p){
+		if(idhoja)
+			cout << "found " << idhoja << "  at position " << p-szhoja << endl;
+		if(get_nxthoja()) get_nxthoja()->print(p);
 	}
-	void ocurrencias(const string &s, int p=0){
-		printocur(p-1);
-		if(s[p]) get_tran(s[p])->ocurrencias(s, p+1);
+	void matching(const string &s, int p=0){
+		print(p);
+		if(s[p]) get_tran(s[p])->matching(s, p+1);
 	}
 }tri;
 
 
 int main(){
 	tri=trie();//clear
-	tri.add("ho", 1);
-	tri.add("hoho", 2);
-	tri.add("hola", 3);
-	tri.ocurrencias("hohohola");
+	tri.insert("ho", 1);
+	tri.insert("hoho", 2);
+	tri.insert("hola", 3);
+	tri.matching("hohohola");
 	return 0;
 }
