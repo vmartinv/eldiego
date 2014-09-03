1ae6c4cacae50d075a8db2418bad44fe75f28447
mvpossum <mvillagra0@gmail.com>
2014-09-03 20:14:03 -0300

    rmq now working

--- estructuras/rmq.dynamic.cpp
+++ estructuras/rmq.dynamic.cpp
@@ -1,7 +1,7 @@
 //Dado un arreglo y una operacion asociativa con neutro, get(i, j) opera sobre el rango [i, j).
 #define MAXN 100000
 struct RMQ{
-	static const int sz=65536;//2*2^ceil(log(n))
+	int sz;
 	tipo t[4*MAXN];
 	tipo &operator[](int p){return t[sz+p];}
 	void init(int n){//O(nlgn)
@@ -9,15 +9,16 @@ struct RMQ{
 		fill(t, t+2*sz, 0); // 0=elemento neutro
 	}
 	void updall(){//O(n)
-		dforn(i, sz) t[i]=max(t[2*i], t[2*i+1]);}
-	tipo get(int i, int j, int n=1, int a=0, int b=sz/2){//O(lgn)
+		dforsn(i, 0, sz) t[i]=max(t[2*i], t[2*i+1]);}
+	tipo get(int i, int j){return get(i,j,1,0,sz);}
+	tipo get(int i, int j, int n, int a, int b){//O(lgn)
 		if(j<=a || i>=b) return 0;//neutro
 		if(i<=a && b<=j) return t[n];
 		int c=(a+b)/2;
 		return max(get(i, j, 2*n, a, c), get(i, j, 2*n+1, c, b));
 	}
 	void set(int p, tipo val){//O(lgn)
-		for(p+=sz/2; p>0 && t[p]!=val;){
+		for(p+=sz; p>0 && t[p]!=val;){
 			t[p]=val;
 			p/=2;
 			val=max(t[p*2], t[p*2+1]);
