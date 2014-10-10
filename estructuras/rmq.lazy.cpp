f48044c16bcccdf06154dd3e137bdb2bfb2dae09
mvpossum <mvillagra0@gmail.com>
2014-10-10 11:39:57 -0300

    lazy improved

--- estructuras/rmq.lazy.cpp
+++ estructuras/rmq.lazy.cpp
@@ -1,9 +1,9 @@
 //Dado un arreglo y una operacion asociativa con neutro, get(i, j) opera sobre el rango [i, j).
-#define MAXN 100000
-typedef int tipo1;
-typedef int tipo2;
-const tipo1 neutro=0;
+typedef int tipo;//tipo de los elementos del arreglo
+typedef int tipo2;//tipo de la alteracion
 #define operacion(x,y) x+y
+const tipo neutro=0; const tipo2 neutro2=0;
+#define MAXN 100000
 struct RMQ{
 	int sz;
 	tipo t[4*MAXN];
@@ -12,24 +12,28 @@ struct RMQ{
 	void init(int n){//O(nlgn)
 		sz = 1 << (32-__builtin_clz(n));
 		forr(i, sz, 2*sz) t[i]=neutro;
-		forn(i, 2*sz) dirty[i]=0;
+		forn(i, 2*sz) dirty[i]=neutro2;
 	}
-	void modify(tipo2 v, int n){//altera un elemento y marca sucio sus hijos
-		t[n]+=v;
-		if(n<sz){
-			dirty[2*n]+=v/2;
-			dirty[2*n+1]+=v/2;
+	tipo2 predict(tipo2 k, int n){//sobre un arreglo de tamanio n
+		return k*n;	}
+	tipo2 acum(tipo2 k1, tipo2 k2){//acumula dos alteraciones
+		return k1+k2;}
+	tipo f(tipo v, tipo2 k){//la alteracion
+		return v+k;}
+		
+	void modify(tipo2 v, int cant, int n){
+		t[n]=f(t[n], predict(v, cant));//altera un nodo 
+		if(n<sz){//y marca sucio sus hijos
+			dirty[2*n  ]=acum(dirty[2*n  ], v);
+			dirty[2*n+1]=acum(dirty[2*n+1], v);
 		}
 	}
-	void correct(int n){//corrige un elemento(sacandole lo dirty)
-		if(dirty[n]){
-			modify(dirty[n], n);
-			dirty[n]=0;
-		}
-	}
+	void correct(int cant, int n){
+		modify(dirty[n], cant, n);
+		dirty[n]=neutro2;}
 	tipo get(int i, int j){return get(i,j,1,0,sz);}
 	tipo get(int i, int j, int n, int a, int b){//O(lgn)
-		correct(n);//corrige el valor antes de usarlo
+		correct(n, b-a);//corrige el valor antes de usarlo
 		if(j<=a || i>=b) return neutro;
 		if(i<=a && b<=j) return t[n];
 		int c=(a+b)/2;
@@ -38,10 +42,10 @@ struct RMQ{
 	//altera los valores en [i, j) con una alteracion de val
 	void set(tipo2 val, int i, int j){set(val,i,j,1,0,sz);}
 	void set(tipo2 val, int i, int j, int n, int a, int b){//O(lgn)
-		correct(n);
+		correct(n, b-a);
 		if(j<=a || i>=b) return;
 		if(i<=a && b<=j){
-			modify((b-a)*val, n);
+			modify(val, b-a, n);
 			return;
 		}
 		int c=(a+b)/2;
