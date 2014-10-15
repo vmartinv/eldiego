771754d3da73feb06307546faa4e449abb993891
mvpossum <mvillagra0@gmail.com>
2014-10-14 22:54:52 -0300

    lazy

--- estructuras/rmq.lazy.cpp
+++ estructuras/rmq.lazy.cpp
@@ -1,55 +1,49 @@
 //Dado un arreglo y una operacion asociativa con neutro, get(i, j) opera sobre el rango [i, j).
-typedef int tipo;//tipo de los elementos del arreglo
-typedef int tipo2;//tipo de la alteracion
+typedef int Elem;//Elem de los elementos del arreglo
+typedef int Alt;//Elem de la alteracion
 #define operacion(x,y) x+y
-const tipo neutro=0; const tipo2 neutro2=0;
+const Elem neutro=0; const Alt neutro2=0;
 #define MAXN 100000
 struct RMQ{
 	int sz;
-	tipo t[4*MAXN];
-	tipo2 dirty[4*MAXN];//las alteraciones pueden ser de distinto tipo
-	tipo &operator[](int p){return t[sz+p];}
+	Elem t[4*MAXN];
+	Alt dirty[4*MAXN];//las alteraciones pueden ser de distinto Elem
+	Elem &operator[](int p){return t[sz+p];}
 	void init(int n){//O(nlgn)
 		sz = 1 << (32-__builtin_clz(n));
 		forr(i, sz, 2*sz) t[i]=neutro;
 		forn(i, 2*sz) dirty[i]=neutro2;
 	}
-	tipo f(tipo v, tipo2 k){//la alteracion
-		return v+k;}
-	tipo2 acum(tipo2 k1, tipo2 k2){//acumula dos alteraciones
-		return k1+k2;}
-	tipo predict(tipo v, tipo2 k, int n){//sobre un arreglo de tamanio n
-		return v+k*n;	}
-		
-	void modify(tipo2 v, int cant, int n){
-		t[n]=predict(t[n], v, cant);//altera un nodo 
-		if(n<sz){//y marca sucio sus hijos
-			dirty[2*n  ]=acum(dirty[2*n  ], v);
-			dirty[2*n+1]=acum(dirty[2*n+1], v);
+	void push(int n, int a, int b){//propaga el dirty a sus hijos
+		if(dirty[n]!=0){
+			t[n]+=dirty[n]*(b-a);//altera el nodo 
+			if(n<sz){
+				dirty[2*n]+=dirty[n];
+				dirty[2*n+1]+=dirty[n];
+			}
+			dirty[n]=0;
 		}
 	}
-	void correct(int cant, int n){
-		modify(dirty[n], cant, n);
-		dirty[n]=neutro2;}
-	tipo get(int i, int j){return get(i,j,1,0,sz);}
-	tipo get(int i, int j, int n, int a, int b){//O(lgn)
-		correct(n, b-a);//corrige el valor antes de usarlo
+	Elem get(int i, int j, int n, int a, int b){//O(lgn)
 		if(j<=a || i>=b) return neutro;
+		push(n, a, b);//corrige el valor antes de usarlo
 		if(i<=a && b<=j) return t[n];
 		int c=(a+b)/2;
 		return operacion(get(i, j, 2*n, a, c), get(i, j, 2*n+1, c, b));
 	}
+	Elem get(int i, int j){return get(i,j,1,0,sz);}
 	//altera los valores en [i, j) con una alteracion de val
-	void set(tipo2 val, int i, int j){set(val,i,j,1,0,sz);}
-	void set(tipo2 val, int i, int j, int n, int a, int b){//O(lgn)
-		correct(n, b-a);
+	void alterar(Alt val, int i, int j, int n, int a, int b){//O(lgn)
+		push(n, a, b);
 		if(j<=a || i>=b) return;
 		if(i<=a && b<=j){
-			modify(val, b-a, n);
+			dirty[n]+=val;
+			push(n, a, b);
 			return;
 		}
 		int c=(a+b)/2;
-		set(val, i, j, 2*n, a, c), set(val, i, j, 2*n+1, c, b);
-		t[n]=operacion(t[2*n], t[2*n+1]);
+		alterar(val, i, j, 2*n, a, c), alterar(val, i, j, 2*n+1, c, b);
+		t[n]=operacion(t[2*n], t[2*n+1]);//por esto es el push de arriba
 	}
+	void alterar(Alt val, int i, int j){alterar(val,i,j,1,0,sz);}
 }rmq;
--- template.cpp
+++ template.cpp
@@ -3,12 +3,11 @@
 #include <cstdlib>
 #include <cstring>
 #include <vector>
-#include <string>
+#include <algorithm>
 using namespace std;
 #define dprint(v) cerr << #v"=" << v << endl //;)
 #define forr(i,a,b) for(int i=(a); i<(b); i++)
 #define forn(i,n) forr(i,0,n)
-#define dforsn(i,a,b) for(int i=(b)-1; i>=a; i--)
 #define forall(it,v) for(typeof(v.begin()) it=v.begin();it!=v.end();++it)
 #define sz(c) ((int)c.size())
 #define zero(v) memset(v, 0, sizeof(v))
@@ -17,11 +16,9 @@ typedef pair<int,int> ii;
 
 
 int main() {
-#ifndef ONLINE_JUDGE
 	freopen("input.in", "r", stdin);
-#endif
 	while(){
-	
+		
 	}
     return 0;
 }
