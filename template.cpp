4de589355e507a472d87bfba2ec2750b584d2b25
mvpossum <mvillagra0@gmail.com>
2014-10-14 23:34:55 -0300

    dforn

--- template.cpp
+++ template.cpp
@@ -8,6 +8,7 @@ using namespace std;
 #define dprint(v) cerr << #v"=" << v << endl //;)
 #define forr(i,a,b) for(int i=(a); i<(b); i++)
 #define forn(i,n) forr(i,0,n)
+#define dforn(i,n) for(int i=n-1; i>=0; i--)
 #define forall(it,v) for(typeof(v.begin()) it=v.begin();it!=v.end();++it)
 #define sz(c) ((int)c.size())
 #define zero(v) memset(v, 0, sizeof(v))
