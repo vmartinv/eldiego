80798a1c873dafadcf781852c91edba806496f95
karupayun <pablozcanalla@gmail.com>
2014-11-03 21:29:11 -0300

    Update combinatorio.cpp

--- math/combinatorio.cpp
+++ math/combinatorio.cpp
@@ -2,3 +2,14 @@ forn(i, MAXN+1){//comb[i][k]=i tomados de a k
 	comb[i][0]=comb[i][i]=1;
 	forr(k, 1, i) comb[i][k]=(comb[i-1][k]+comb[i-1][k-1])%MOD;
 }
+
+ll lucas (ll n, ll k, int p){ //Calcula (n,k)%p teniendo comb[p][p] precalculado.
+	ll aux = 1;
+	while (n + k){
+		aux = (aux * comb[n%p][k%p]) %p;
+		n/=p, k/=p;
+	}
+	return aux;
+}
+
+
