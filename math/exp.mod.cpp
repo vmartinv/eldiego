6d8d625dfeea7ef39a0eabe980e367e7c51afd14
mvpossum <mvillagra0@gmail.com>
2014-11-03 23:01:04 -0300

    expmod fix

--- math/exp.mod.cpp
+++ math/exp.mod.cpp
@@ -1,6 +1,6 @@
 #define MOD 1000000007
 ll expmod (ll b, ll e, ll m){//O(log b)
 	if(!e) return 1;
-	ll q= expmod(b,e/2); q=(q*q)%m;
+	ll q= expmod(b,e/2,m); q=(q*q)%m;
 	return e%2? (b * q)%m : q;
 }
