e8961e650b6386471e57a27f65859c80c253c2ff
mvpossum <mvillagra0@gmail.com>
2014-11-06 13:23:17 -0300

    interpol

--- math/polinomio.cpp
+++ math/polinomio.cpp
@@ -1,31 +1,54 @@
-#define MAX_GR  20
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <set>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+#define dprint(v) cerr << #v"=" << v << endl //;)
+#define forr(i,a,b) for(int i=(a); i<(b); i++)
+#define forn(i,n) forr(i,0,n)
+#define dforn(i,n) for(int i=n-1; i>=0; i--)
+#define forall(it,v) for(typeof(v.begin()) it=v.begin();it!=v.end();++it)
+#define sz(c) ((int)c.size())
+#define zero(v) memset(v, 0, sizeof(v))
+#define pb push_back
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef int tipo;
+
 struct poly {
-	tipo p[MAX_GR];//guarda los coeficientes del polinomio
-	poly(){zero(p);}
+    vector<tipo> c;//guarda los coeficientes del polinomio
+    poly(const vector<tipo> &c): c(c) {}
+    poly() {}
 	int gr(){//calculates grade of the polynomial
-		dforn(i,MAX_GR) if(p[i]) return i;
-		return 0; }
-	bool isnull() {return gr()==0 && !p[0];}
-	poly operator+(poly b) {// - is analogous
-		poly c=THIS;
-		forn(i,MAX_GR) c.p[i]+=b.p[i];
-		return c;
-	}
-	poly operator*(poly b) {
-		poly c;
-		forn(i,MAX_GR) forn(k,i+1) c.p[i]+=p[k]*b.p[i-k];
-		return c;
-	}
+		return sz(c); }
+	bool isnull() {return c.empty();}
+    poly operator+(const poly &o) const {
+        int m = sz(c), n = sz(o.c);
+        vector<tipo> res(max(m,n));
+        forn(i, m) res[i] += c[i];
+        forn(i, n) res[i] += o.c[i];
+        return poly(res);
+    }
+    poly operator*(const poly &o) const {
+        int m = sz(c), n = sz(o.c);
+        vector<tipo> res(m+n-1);
+        forn(i, m) forn(j, n) res[i+j]+=c[i]*o.c[j];
+        return poly(res);
+    }
 	tipo eval(tipo v) {
 		tipo sum = 0;
-		dforn(i, MAX_GR) sum=sum*v + p[i];
+		forall(it, c) sum=sum*v + *it;
 		return sum;
 	}
 	//the following function generates the roots of the polynomial
 //it can be easily modified to return float roots
 	set<tipo> roots(){
 		set<tipo> roots;
-		tipo a0 = abs(p[0]), an = abs(p[gr()]);
+		tipo a0 = abs(c[0]), an = abs(c[sz(c)-1]);
 		vector<tipo> ps,qs;
 		forr(p,1,sqrt(a0)+1) if (a0%p==0) ps.pb(p),ps.pb(a0/p);
 		forr(q,1,sqrt(an)+1) if (an%q==0) qs.pb(q),qs.pb(an/q);
@@ -37,6 +60,19 @@ struct poly {
 		return roots;
 	}
 };
+poly interpolate(const vector<tipo> &x, const vector<tipo> &y) {
+    int n = sz(x);
+    poly p;
+    vector<tipo> aux(2);
+    forn(i, n) {
+       double a = y[i] - p.eval(x[i]);
+       forn(j, i) a /= x[i] - x[j];
+       poly add(vector<tipo>(1, a));
+       forn(j, i) aux[0]=-x[j], aux[1]=1, add = add*aux;
+       p = p + add;
+    }
+    return p;
+}
 //the following functions allows parsing an expression like
 //34+150+4*45
 //into a polynomial(el numero en funcion de la base)
@@ -44,7 +80,7 @@ struct poly {
 #define POP(s) s.erase(--s.end());
 poly D(string &s) {
 	poly d;
-	for(int i=0; isdigit(LAST(s)); i++) d.p[i]=LAST(s)-'0', POP(s);
+	for(int i=0; isdigit(LAST(s)); i++) d.c.push_back(LAST(s)-'0'), POP(s);
 	return d;}
 
 poly T(string &s) {
@@ -57,4 +93,8 @@ poly E(string &s) {
 	poly e=T(s);
 	if (LAST(s)=='+'){POP(s); return E(s)+e;}
 	return e;
+}
+
+int  main(){ 
+	return 0;
 }
