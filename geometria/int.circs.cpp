891000a4c8e0dd7daccf3827dff3da78e438759f
karupayun <pablozcanalla@gmail.com>
2015-05-13 02:48:38 -0300

    Create int.circs.cpp

---
+++ geometria/int.circs.cpp
@@ -0,0 +1,60 @@
+struct event {
+    double x; int t;
+    event(double xx, int tt) : x(xx), t(tt) {}
+    bool operator <(const event &o) const { return x < o.x; }
+};
+typedef vector<Circle> VC;
+typedef vector<event> VE;
+int n;
+double cuenta(VE &v, double A,double B) {
+    sort(v.begin(), v.end());
+    double res = 0.0, lx = ((v.empty())?0.0:v[0].x);
+    int contador = 0;
+    forn(i,sz(v)) {
+        // interseccion de todos (contador == n), union de todos (contador > 0),
+        // conjunto de puntos cubierto por exacta k Circulos (contador == k)
+        if (contador == n) res += v[i].x - lx;
+        contador += v[i].t;
+        lx = v[i].x;
+    }
+    return res;
+}
+// Primitiva de sqrt(r*r - x*x) como funcion double de una variable x.
+inline double primitiva(double x,double r) {
+    if (x >= r) return r*r*M_PI/4.0;
+    if (x <= -r) return -r*r*M_PI/4.0;
+    double raiz = sqrt(r*r-x*x);
+    return 0.5 * (x * raiz + r*r*atan(x/raiz));
+}
+double interCircle(VC &v) {
+    vector<double> p; p.reserve(v.size() * (v.size() + 2));
+    forn(i,sz(v)) {
+        p.push_back(v[i].c.x + v[i].r);
+        p.push_back(v[i].c.x - v[i].r);
+    }
+    forn(i,sz(v)) forn(j,i) {
+        Circle &a = v[i], b = v[j];
+        double d = (a.c - b.c).norm();
+        if (fabs(a.r - b.r) < d && d < a.r + b.r) {
+            double alfa = acos((sqr(a.r) + sqr(d) - sqr(b.r)) / (2.0 * d * a.r));
+            pto vec = (b.c - a.c) * (a.r / d);
+            p.pb((a.c + rotate(vec, alfa)).x);
+            p.pb((a.c + rotate(vec, -alfa)).x);
+        }
+    }
+    sort(p.begin(), p.end());
+    double res = 0.0;
+    forn(i,sz(p)-1) {
+        const double A = p[i], B = p[i+1];
+        VE ve; ve.reserve(2 * v.size());
+        forn(j,sz(v)) {
+            const Circle &c = v[j];
+            double arco = primitiva(B-c.c.x,c.r) - primitiva(A-c.c.x,c.r);
+            double base = c.c.y * (B-A);
+            ve.push_back(event(base + arco,-1));
+            ve.push_back(event(base - arco, 1));
+        }
+        res += cuenta(ve,A,B);
+    }
+    return res;
+}
