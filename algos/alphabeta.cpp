73faa86442cbac89a9e3950b842b2a54a5871166
mariano22 <marianocrosetti1993@gmail.com>
2015-05-13 15:12:41 -0300

    Create alphabeta.cpp

---
+++ algos/alphabeta.cpp
@@ -0,0 +1,14 @@
+ll alphabeta(State &s, bool player = true, int depth = 1e9, ll alpha = -INF, ll beta = INF) { //player = true -> Maximiza
+    if(s.isFinal()) return s.score;
+	//~ if (!depth) return s.heuristic();
+    vector<State> children;
+    s.expand(player, children);
+    int n = children.size();
+    forn(i, n) {
+        ll v = alphabeta(children[i], !player, depth-1, alpha, beta);
+        if(!player) alpha = max(alpha, v);
+        else beta = min(beta, v);
+        if(beta <= alpha) break;
+    }
+    return !player ? alpha : beta;
+}
