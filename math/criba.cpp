3c86c6d2a52e17630eccbddebf262631413cb8e9
Martín Villagra <mvillagra0@gmail.com>
2015-08-30 15:30:27 -0300

    Update criba.cpp

--- math/criba.cpp
+++ math/criba.cpp
@@ -10,7 +10,6 @@ using namespace std;
 #define dprint(v) cerr << #v"=" << v << endl //;)
 #define forr(i,a,b) for(int i=(a); i<(b); i++)
 #define forn(i,n) forr(i,0,n)
-
 #define forall(it,v) for(typeof(v.begin()) it=v.begin();it!=v.end();++it)
 #define sz(c) ((int)c.size())
 #define zero(v) memset(v, 0, sizeof(v))
@@ -19,20 +18,24 @@ typedef pair<int,int> ii;
 
 #define MAXP 80000	//no necesariamente primo
 int criba[MAXP+1];
-vector<int> primos;
-void buscarprimos(){
-	int sq=sqrt(MAXP)+1;
-	forr(p, 2, MAXP+1) if(!criba[p]){
-		primos.push_back(p);
-		if(p<=sq)
-			for(int m=p*p; m<=MAXP; m+=p)//borro los multiplos de p
-				if(!criba[m])criba[m]=p;
-	}
+void crearcriba(){
+	int w[] = {4,2,4,2,4,6,2,6};
+	for(int p=25;p<=MAXP;p+=10) criba[p]=5;
+	for(int p=9;p<=MAXP;p+=6) criba[p]=3; 
+	for(int p=4;p<=MAXP;p+=2) criba[p]=2;
+	for(int p=7,cur=0;p*p<=MAXP;p+=w[cur++&7]) if (!criba[p]) 
+		for(int j=p*p;j<=MAXP;j+=(p<<1)) if(!criba[j]) criba[j]=p;
 }
+//~ Useful for bit trick: 
+//~ #define SET(i) ( criba[i>>5]= criba[i>>5] | (1<<(i&31)) )
+//~ #define INDEX(i) ((criba[i>>5]>>(i&31))&1)
+//~ unsigned int criba[(MAXP/32)+1];
 
+vector<int> primos;
 int main() {
 	freopen("primos", "w", stdout);
-	buscarprimos();
+	crearcriba();
+	for(int i=2;i<=MAXP;i++) if (!criba[i]) primos.push_back(i);
 	cout << '{';
 	bool first=true;
 	forall(it, primos){
