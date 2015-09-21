e51722cbfeb975f05ea06f005c5c4141bbcf0621
Oia Poli <politecnico.oia@gmail.com>
2015-09-21 17:10:33 -0300

    generador divisores

--- math/factorizacion.cpp
+++ math/factorizacion.cpp
@@ -2,18 +2,24 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <cassert>
-#include <vector>
-#include <map>
 #include <cmath>
-#include <string>
+#include <vector>
+#include <cassert>
+#include <unistd.h>
+#include <map>
+#include <set>
+#include <algorithm>
 using namespace std;
 #define dprint(v) cerr << #v"=" << v << endl //;)
 #define forr(i,a,b) for(int i=(a); i<(b); i++)
 #define forn(i,n) forr(i,0,n)
+#define dforn(i,n) for(int i=n-1; i>=0; i--)
 #define forall(it,v) for(typeof(v.begin()) it=v.begin();it!=v.end();++it)
 #define sz(c) ((int)c.size())
 #define zero(v) memset(v, 0, sizeof(v))
+#define pb push_back
+#define fst first
+#define snd second
 typedef long long ll;
 typedef pair<int,int> ii;
 
@@ -57,20 +63,30 @@ map<ll,ll> fact2(ll n){ //O (lg n)
 map<ll,ll> f3;
 void fact3(ll n){ //O (lg n)^3. un solo numero
     if (n == 1) return;
-    if (miller_rabin(n))
+    if (rabin(n))
         f3[n]++;
     else{
-        ll aux = pollard_rho(n);
-        fac3(aux); fac3(n/aux);        
+        ll aux = rho(n);
+        fact3(aux); fact3(n/aux);        
     }
 	if(n>1) f3[n]++;
 	return;
 }
 
+//Usar asi:  divisores(fac, divs, fac.begin()); NO ESTA ORDENADO
+void divisores(const map<ll,ll> &f, vector<ll> &divs, map<ll,ll>::iterator it, ll n=1){
+    if(it==f.begin()) divs.clear();
+    if(it==f.end()) {
+        if(n>1) divs.pb(n);
+        return;
+    }
+    ll p=it->fst, k=it->snd; ++it;
+    forn(_, k+1)
+        divisores(f, divs, it, n), n*=p;
+}
+
 int main() {
 	buscarprimos();
-	int x=476928;
-	//~ cout << x << " = ";
 	forr(x, 1, 400000){
 		dprint(x);
 		map<ll,ll> f=fact(x);
@@ -79,6 +95,11 @@ int main() {
 			assert(sz(f)==sz(f2));
 			forall(it, f) assert(f2[it->first]==it->second);
 		}
+        //muestra divisores
+        vector<ll> d;
+        divisores(f, d, f.begin());
+        forall(it, d) cout << *it << ' '; cout  << endl;
+        sleep(1);
 	}
     return 0;
 }
