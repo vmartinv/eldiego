4d0df88f4412bd01f7df8c1623f535bc72c6c792
mvpossum <mvillagra0@gmail.com>
2015-09-23 11:42:29 -0300

    line side

--- geometria/line.cpp
+++ geometria/line.cpp
@@ -1,9 +1,11 @@
+int sgn(ll x){return x<0? -1 : !!x;}
 struct line{
 	line() {}
 	double a,b,c;//Ax+By=C
 //pto MUST store float coordinates!
 	line(double a, double b, double c):a(a),b(b),c(c){}
 	line(pto p, pto q): a(q.y-p.y), b(p.x-q.x), c(a*p.x+b*p.y) {}
+	int side(pto p){return sgn(ll(a) * p.x + ll(b) * p.y - c);}
 };
 bool parallels(line l1, line l2){return abs(l1.a*l2.b-l2.a*l1.b)<EPS;}
 pto inter(line l1, line l2){//intersection
