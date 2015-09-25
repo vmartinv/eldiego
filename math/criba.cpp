65e53fd1962ecc9b486348f3a68ce34045bacaca
Pablo Zimmermann <pablozcanalla@gmail.com>
2015-09-24 22:46:26 -0300

    bug de criba+buscarp

--- math/criba.cpp
+++ math/criba.cpp
@@ -16,22 +16,29 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int> ii;
 
-#define MAXP 80000	//no necesariamente primo
+#define MAXP 100000	//no necesariamente primo
 int criba[MAXP+1];
 void crearcriba(){
 	int w[] = {4,2,4,2,4,6,2,6};
 	for(int p=25;p<=MAXP;p+=10) criba[p]=5;
 	for(int p=9;p<=MAXP;p+=6) criba[p]=3; 
 	for(int p=4;p<=MAXP;p+=2) criba[p]=2;
-	for(int p=7,cur=0;p*p<=MAXP;p+=w[cur++&7]) if (!criba[p]) 
+	for(int p=7,cur=0;(ll)p*p<=MAXP;p+=w[cur++&7]) if (!criba[p]) 
 		for(int j=p*p;j<=MAXP;j+=(p<<1)) if(!criba[j]) criba[j]=p;
 }
+
+vector<int> primos;
+void buscarprimos(){
+	crearcriba();
+	forr (i,2,MAXP+1) if (!criba[i]) primos.push_back(i);
+}
+
 //~ Useful for bit trick: 
 //~ #define SET(i) ( criba[i>>5]= criba[i>>5] | (1<<(i&31)) )
 //~ #define INDEX(i) ((criba[i>>5]>>(i&31))&1)
 //~ unsigned int criba[(MAXP/32)+1];
 
-vector<int> primos;
+
 int main() {
 	freopen("primos", "w", stdout);
 	crearcriba();
