36ebd7cc69c438069fecbcbbdf9afb855b0fc5de
Martin Villagra <mvillagra0@hotmail.com>
2015-09-29 09:34:17 -0300

    order statics

---
+++ estructuras/order.tree.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define dprint(v) cerr << #v"=" << v << endl //;)
+#define forr(i,a,b) for(int i=(a); i<(b); i++)
+#define forn(i,n) forr(i,0,n)
+#define dforn(i,n) for(int i=n-1; i>=0; i--)
+#define forall(it,v) for(typeof(v.begin()) it=v.begin();it!=v.end();++it)
+#define sz(c) ((int)c.size())
+#define zero(v) memset(v, 0, sizeof(v))
+#define pb push_back
+#define fst first
+#define snd second
+typedef long long ll;
+typedef pair<int,int> ii;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace  __gnu_pbds;
+typedef tree<int,null_type,less<int>,//key,mapped type, comparator
+    rb_tree_tag,tree_order_statistics_node_update> set_t;
+//find_by_order(i) devuelve iterador al i-esimo elemento
+//order_of_key(k): devuelve la pos del lower bound de k
+//Ej: 12, 100, 505, 1000, 10000.
+//order_of_key(10) == 0, order_of_key(100) == 1,
+//order_of_key(707) == 3, order_of_key(9999999) == 5
+
+int main()
+{
+    
+    
+    return 0;
+}
