9840fbcd771bec8b6dc5fef6fcd1e45cf6b97d34
Oia Poli <politecnico.oia@gmail.com>
2015-11-04 00:22:40 -0300

    exp mod para matrices grandes

--- math/exp.mat.cpp
+++ math/exp.mat.cpp
@@ -1,3 +1,20 @@
+#define SIZE 350
+int NN;
+void mul(double a[SIZE][SIZE], double b[SIZE][SIZE])
+{
+    double res[SIZE][SIZE] = {{0}};
+    forn(i, NN) forn(j, NN) forn(k, NN) res[i][j]+=a[i][k]*b[k][j];
+    forn(i, NN) forn(j, NN) a[i][j]=res[i][j];
+}
+void powmat(double a[SIZE][SIZE], int n, double res[SIZE][SIZE])
+{
+    forn(i, NN) forn(j, NN) res[i][j]=(i==j);
+    while(n){
+        if(n&1) mul(res, a), n--;
+        else mul(a, a), n/=2;
+    }
+}
+
 struct M22{		   // |a b|
 	tipo a,b,c,d;// |c d|
 	M22 operator*(const M22 &p) const {
