bc7021f9f1f180c6d08d54d50b2c87aafbb7d41c
Oia Poli <politecnico.oia@gmail.com>
2015-11-09 20:27:13 -0300

    determinante matrix struct

---
+++ math/determinante.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define dprint(v) cerr << #v"=" << v << endl //;)
+#define forr(i,a,b) for(int i=(a); i<(b); i++)
+#define forn(i,n) forr(i,0,n)
+#define dforn(i,n) for(int i=n-1; i>=0; i--)
+#define forall(it,v) for(typeof(v.begin()) it=v.begin();it!=v.end();++it)
+#define sz(c) ((int)c.size())
+#define zero(v) memset(v, 0, sizeof(v))
+#define pb push_back
+#define fst first
+#define snd second
+typedef long long ll;
+typedef pair<int,int> ii;
+
+
+struct Mat {
+    vector<vector<double> > vec;
+    Mat(int n): vec(n, vector<double>(n) ) {}
+    Mat(int n, int m): vec(n, vector<double>(m) ) {}
+    vector<double> &operator[](int f){return vec[f];}
+    const vector<double> &operator[](int f) const {return vec[f];}
+    int size() const {return sz(vec);}
+    Mat operator+(Mat &b) { ///this de n x m entonces b de n x m
+        Mat m(sz(b),sz(b[0]));
+        forn(i,sz(vec)) forn(j,sz(vec[0])) m[i][j] = vec[i][j] + b[i][j];
+        return m;
+    }
+    Mat operator*(const Mat &b) { ///this de n x m entonces b de m x t
+        int n = sz(vec), m = sz(vec[0]), t = sz(b[0]);
+        Mat mat(n,t);
+        forn(i,n) forn(j,t) {
+            forn(k,m)
+                mat[i][j] += vec[i][k] * b[k][j];
+        }
+        return mat;
+    }
+    double determinant(){//sacado de e maxx ru
+        double det = 1;
+        int n = sz(vec);
+        Mat m(*this);
+        forn(i, n){//para cada columna
+            int k = i;
+            forr(j, i+1, n)//busco la fila con mayor val abs
+                if(abs(m[j][i])>abs(m[k][i]))
+                    k = j;
+            if(abs(m[k][i])<1e-9) return 0;
+            m[i].swap(m[k]);//la swapeo
+            if(i!=k) det = -det;
+            det *= m[i][i];
+            forr(j, i+1, n) m[i][j] /= m[i][i];
+            //hago 0 todas las otras filas
+            forn(j, n) if (j!= i && abs(m[j][i])>1e-9)
+                forr(k, i+1, n) m[j][k]-=m[i][k]*m[j][i];
+        }
+        return det;
+    }
+    Mat identidad(int n) {
+        Mat m(n);
+        forn(i,n) m[i][i] = 1;
+        return m;
+    }
+    Mat transpuesta() {
+        Mat m(sz(vec[0]),sz(vec));
+        forn(i,sz(vec[0])) forn(j,sz(vec))
+            m[i][j] = vec[j][i];
+        return m;
+    }
+    void print() {
+        int n = sz(vec), m = sz(vec[0]);
+        cout << "***************" << endl;
+        forn(i,n){
+            forn(j,m) cout << " "+!j << vec[i][j];
+            cout << endl;
+        }
+    }
+};
+
+int n;
+int main() {
+//DETERMINANTE:
+//https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=625
+	freopen("input.in", "r", stdin);
+    ios::sync_with_stdio(0);
+    while(cin >> n && n){
+        Mat m(n);
+        forn(i, n) forn(j, n) cin >> m[i][j];
+        cout << (ll)round(m.determinant()) << endl;
+    }
+    cout  << "*" << endl;
+	return 0;
+}
