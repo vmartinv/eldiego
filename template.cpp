a7cb9ce578a9f3af30ab5596e2dd7f7d29b34d19
Martín Villagra <mvillagra0@gmail.com>
2016-02-21 19:08:39 -0300

    C++11 Template!

--- template.cpp
+++ template.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define forr(i,a,b) for(int i=(a); i<(b); i++)
 #define forn(i,n) forr(i,0,n)
 #define dforn(i,n) for(int i=n-1; i>=0; i--)
-#define forall(it,v) for(typeof(v.begin()) it=v.begin();it!=v.end();++it)
+#define forall(it,v) for(auto it=v.begin();it!=v.end();++it)
 #define sz(c) ((int)c.size())
 #define zero(v) memset(v, 0, sizeof(v))
 #define pb push_back
@@ -17,10 +17,10 @@ const int MAXN=100100;
 int n;
 
 int main() {
-	freopen("input.in", "r", stdin);
+    freopen("input.in", "r", stdin);
     ios::sync_with_stdio(0);
-	while(cin >> n){
-		
-	}
-	return 0;
+    while(cin >> n){
+    
+    }
+    return 0;
 }
