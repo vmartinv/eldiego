f91349193cabaa89a3ea34d85dfef1a1f7220bba
Martín Villagra <mvillagra0@gmail.com>
2016-02-23 17:21:44 -0300

    fixed inconsistent updates

--- algos/mosalgortihm.cpp
+++ algos/mosalgortihm.cpp
@@ -1,23 +1,25 @@
-int sq,n;//n=cantidad de elementos, sq=sqrt(n);
-struct Qu{//queries
+int n,sq;
+struct Qu{//queries [l, r]
     //intervalos cerrado abiertos !!! importante!!
-    int l, r, id;//qs[i].id=i (orden de impresion)
-    ll ans;//respuesta de la querie
+    int l, r, id;
 }qs[MAXN];
+int ans[MAXN], curans;//ans[i]=ans to ith query
 bool bymos(const Qu &a, const Qu &b){
     if(a.l/sq!=b.l/sq) return a.l<b.l;
-	return (a.l/sq)&1? a.r<b.r : a.r>b.r;
+    return (a.l/sq)&1? a.r<b.r : a.r>b.r;
 }
 void mos(){
+    forn(i, t) qs[i].id=i;
     sort(qs, qs+t, bymos);
     int cl=0, cr=0; 
+    sq=sqrt(n);
+    curans=0;
     forn(i, t){ //intervalos cerrado abiertos !!! importante!!
         Qu &q=qs[i];
-        while(cl<q.l) remove(cl++);
         while(cl>q.l) add(--cl);
         while(cr<q.r) add(cr++);
+        while(cl<q.l) remove(cl++);
         while(cr>q.r) remove(--cr);
-        q.ans=ans;
+        ans[q.id]=curans;
     }
-    sort(qs, qs+t, byid);
 }
