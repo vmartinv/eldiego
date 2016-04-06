4fd39bc8dafda8a94084939192b18dced1c8572e
Oia Poli <politecnico.oia@gmail.com>
2016-04-06 15:28:21 -0300

    improved traparr

--- estructuras/treaparr.cpp
+++ estructuras/treaparr.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define forr(i,a,b) for(int i=(a); i<(b); i++)
 #define forn(i,n) forr(i,0,n)
 #define dforn(i,n) for(int i=n-1; i>=0; i--)
-#define forall(it,v) for(typeof(v.begin()) it=v.begin();it!=v.end();++it)
+#define forall(it,v) for(auto it=v.begin();it!=v.end();++it)
 #define sz(c) ((int)c.size())
 #define zero(v) memset(v, 0, sizeof(v))
 #define pb push_back
@@ -13,22 +13,31 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int> ii;
 
-
-typedef int Value;
+typedef ii Value;//ii(profundidad, nodo)
 typedef struct node *pnode;
 struct node{
-    Value val;
+    Value val, mini;
+    int dirty;
     int prior, size;
-    pnode l,r;
-    node(Value val=0): val(val), prior(rand()), size(1), l(0), r(0) {}
+    pnode l,r,parent;
+    node(Value val): val(val), mini(val), dirty(0), prior(rand()), size(1), l(0), r(0), parent(0) {}
 };
 static int size(pnode p) { return p ? p->size : 0; } 
-void push(pnode p) {
-	// modificar y propagar el dirty a los hijos aca(para lazy)
+void push(pnode p) {//propagar dirty a los hijos(aca para lazy)
+	p->val.fst+=p->dirty;
+	p->mini.fst+=p->dirty;
+	if(p->l) p->l->dirty+=p->dirty;
+	if(p->r) p->r->dirty+=p->dirty;
+	p->dirty=0;
 }
+static Value mini(pnode p) { return p ? push(p), p->mini : ii(1e9, -1); } 
 // Update function and size from children's Value
 void pull(pnode p) {//recalcular valor del nodo aca (para rmq)
 	p->size = 1 + size(p->l) + size(p->r);
+	p->mini = min(min(p->val, mini(p->l)), mini(p->r));//operacion del rmq!
+	p->parent=0;
+	if(p->l) p->l->parent=p;
+	if(p->r) p->r->parent=p;
 }
 //junta dos arreglos
 pnode merge(pnode l, pnode r) {
@@ -54,66 +63,88 @@ pnode at(pnode t, int pos) {
 	if(pos < size(t->l)) return at(t->l, pos);
 	return at(t->r, pos - 1 - size(t->l));
 }
-
-ostream& operator<<(ostream &out, const pnode &t) {
-	if(!t) return out;
-    return out << t->l << t->val << ' ' << t->r;
+int getpos(pnode t){//inversa de at
+	if(!t->parent) return size(t->l);
+	if(t==t->parent->l) return getpos(t->parent)-size(t->r)-1;
+	return getpos(t->parent)+size(t->l)+1;
+}
+void split(pnode t, int i, int j, pnode &l, pnode &m, pnode &r) {
+	split(t, i, l, t), split(t, j-i, m, r);}
+Value get(pnode &p, int i, int j){//like rmq
+	pnode l,m,r;
+    split(p, i, j, l, m, r);
+    Value ret=mini(m);
+    p=merge(l, merge(m, r));
+    return ret;
+}
+void print(const pnode &t) {//for debugging
+	if(!t) return;
+    push(t);
+    print(t->l);
+    cout << t->val.fst << ' ';
+    print(t->r);
 }
 
-
-struct arr {//para usar el treap como un arreglo
-    pnode root;
-    arr(Value val): root(new node(val)) {}
-    arr(pnode root=0): root(root) {}
-    void insert(int pos, Value val) {//agrega un elemento
-        pnode t1,t2; ::split(root, pos, t1, t2);
-        t1=merge(t1, new node(val));
-        root=merge(t1,t2);
-    }
-    void erase(int i, int j=-1) {//borra un rango
-		if(j==-1) j=i+1;
-        pnode t1,t2,t3;
-        ::split(root,i,t1,t2);
-        ::split(t2,j-i,t2,t3);
-        root=merge(t1, t3);
-    }
-    void push_back(Value val) {root=merge(root, new node(val));}
-    Value &operator[](int pos){return at(root, pos)->val;}//ojito
-    //parte el arreglo en dos con sz(l)==tam
-    void split(int tam, arr &l, arr &r){::split(root, tam, l.root, r.root);}
-    //dado un rango, parte el arreglo en tres. m = rango [i, j)
-    void split(int i, int j, arr &l, arr &m, arr &r){
-		::split(root, i, l.root, m.root);
-		::split(m.root, j-i, m.root, r.root);
+//Sample program: C. LCA Online from Petrozavodsk Summer-2012. Petrozavodsk SU Contest
+//Available at http://opentrains.snarknews.info/~ejudge
+const int MAXN=300100;
+int n;
+pnode beg[MAXN], fin[MAXN];
+pnode lista;
+vector<int> G[MAXN];
+void euler(int v, int L=0){
+	fin[v]=beg[v]=new node(ii(L, v));
+	lista=merge(lista, fin[v]);
+	forall(it, G[v]){
+		euler(*it, L+1);
+		fin[v]=new node(ii(L, v));
+		lista=merge(lista, fin[v]);
 	}
-};
-//concatena dos arreglos
-arr merge(arr a, arr b){return arr(merge(a.root, b.root));}
+}
 
+int query(int u, int v){
+	int pu=getpos(beg[u]), pv=getpos(beg[v]);
+	if(pu>pv) return query(v, u);
+	pnode l,m,r;
+	split(lista, pu, pv+1, l, m, r);
+	int ret=mini(m).snd;
+	lista=merge(merge(l, m),r);
+    return ret;
+}
 
-int main(){
-    arr t;
-    int x;
-    forn(_, 10) t.pb(rand()%10);
-    cout << t.root << endl;
-    while(cin >> x) {
-        //~ if (x == 0) break;
-        //~ else if (x > 0) t.pb(x);
-        //~ else{
-            //~ x = -x;
-            //~ t.erase(x);
-        //~ }
-        
-        //~ pnode q;
-        int y; cin >> y;
-        arr l, m, r;
-        t.split(x, y, l, m, r);
-        cout << l.root << endl;
-        cout << m.root << endl;
-        cout << r.root << endl;
-        t=merge(l, m);
-        t=merge(t, r);
-        cout << t.root << endl;
+void change(int u, int nuparent){
+	int b=getpos(beg[u]), e=getpos(fin[u]);
+    int pnu=getpos(beg[nuparent]);
+	int lv=get(lista, pnu, pnu+1).fst;
+	pnode l,m,r;
+	split(lista, b, e+1, l, m, r);
+	m->dirty=-get(m, 0, 1).fst+lv+1;
+	lista=merge(l, r);
+	split(lista, getpos(fin[nuparent])+1, l, r);
+	fin[nuparent]=new node(ii(lv, nuparent));
+	lista=merge(merge(l, m), merge(fin[nuparent], r));
+}
+
+int main() {
+    //~ freopen("f.in", "r", stdin);
+    freopen("lca.in", "r", stdin);
+    freopen("lca.out", "w", stdout);
+    ios::sync_with_stdio(0);
+    while(cin >> n){
+		zero(G);
+		forn(i, n-1){
+			int x; cin >>x; x--;
+			G[x].pb(i+1);
+		}
+		lista=0;
+		euler(0);
+		int m; cin >> m;
+		while(m--){
+			char ty[10]; int u,v;
+			cin >> ty >> u >> v; u--, v--;
+			if(ty[0]=='Q')   cout << query(u, v)+1 << endl;
+            else change(u, v);
+		}
     }
     return 0;
 }
