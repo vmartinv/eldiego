f7416f3ebfa6892adc618efc600579ddea3eba84
mvpossum <mvillagra0@gmail.com>
2016-04-18 11:13:19 -0300

    convex hull aclaration

--- geometria/convex.hull.cpp
+++ geometria/convex.hull.cpp
@@ -2,13 +2,13 @@
 void CH(vector<pto>& P, vector<pto> &S){
 	S.clear();
 	sort(P.begin(), P.end());
-	forn(i, sz(P)){
+	forn(i, sz(P)){//left must return >=0 to delete collinear points!
 		while(sz(S)>= 2 && S[sz(S)-1].left(S[sz(S)-2], P[i])) S.pop_back();
 		S.pb(P[i]);
 	}
 	S.pop_back();
 	int k=sz(S);
-	dforn(i, sz(P)){
+	dforn(i, sz(P)){//left must return >=0 to delete collinear points!
 		while(sz(S) >= k+2 && S[sz(S)-1].left(S[sz(S)-2], P[i])) S.pop_back();
 		S.pb(P[i]);
 	}
