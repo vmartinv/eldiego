81c138e8651f10f4f1a7dc5e203566f379d127ce
mariano22 <marianocrosetti1993@gmail.com>
2016-04-27 20:02:23 -0300

    zfunction base

---
+++ string/zfunction.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define dprint(v) cerr << #v"=" << v << endl //;)
+#define forr(i,a,b) for(int i=(a); i<(b); i++)
+#define forn(i,n) forr(i,0,n)
+#define dforn(i,n) for(int i=n-1; i>=0; i--)
+#define forall(it,v) for(auto it=v.begin();it!=v.end();++it)
+#define sz(c) ((int)c.size())
+#define zero(v) memset(v, 0, sizeof(v))
+#define pb push_back
+#define fst first
+#define snd second
+typedef long long ll;
+typedef pair<int,int> ii;
+
+const int MAXN=100100;
+char s[MAXN];
+int z[MAXN];
+void z_function(char s[],int z[]) {
+    int n = strlen(s);
+    forn(i, n) z[i]=0;
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+        if (i <= r)
+            z[i] = min (r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r)
+            l = i, r = i + z[i] - 1;
+    }
+}
+
+int main() {
+    freopen("input.in", "r", stdin);
+    ios::sync_with_stdio(0);
+    while(cin >> n){
+    
+    }
+    return 0;
+}
