ebc19a1f15a36870fa4a10ca9e62f2a4dafdb204
Martin Villagra <mvillagra0@hotmail.com>
2016-04-29 18:16:35 -0300

    added comments

--- geometria/convex.hull.cpp
+++ geometria/convex.hull.cpp
@@ -1,14 +1,14 @@
 //stores convex hull of P in S, CCW order
 void CH(vector<pto>& P, vector<pto> &S){
 	S.clear();
-	sort(P.begin(), P.end());
-	forn(i, sz(P)){//left must return >=0 to delete collinear points!
+	sort(P.begin(), P.end());//first x, then y
+	forn(i, sz(P)){//lower hull
 		while(sz(S)>= 2 && S[sz(S)-1].left(S[sz(S)-2], P[i])) S.pop_back();
 		S.pb(P[i]);
 	}
 	S.pop_back();
 	int k=sz(S);
-	dforn(i, sz(P)){//left must return >=0 to delete collinear points!
+	dforn(i, sz(P)){//upper hull
 		while(sz(S) >= k+2 && S[sz(S)-1].left(S[sz(S)-2], P[i])) S.pop_back();
 		S.pb(P[i]);
 	}
