4919f4cbd37b58efcda2c905df8a4aabd7a6035f
Martin Villagra <mvillagra0@hotmail.com>
2016-04-29 18:15:11 -0300

    renamed convex hull dynamic

--- estructuras/convexhull.trick.dyn.cpp
+++ estructuras/convexhull.trick.dyn.cpp
@@ -1,19 +1,9 @@
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <vector>
-#include <set>
-#include <cmath>
-#include <map>
-#include <algorithm>
-#include <string>
 #include <bits/stdc++.h>
 using namespace std;
-#define dprint(v) cerr << #v"=" << v << endl
+#define dprint(v) cout << #v"=" << v << endl
 #define forr(i, a, b) for(int i=a; i<(b); i++)
 #define forn(i, n) forr(i, 0, n)
-#define forall(it, v) for(typeof(v.begin()) it=v.begin(); it!=v.end(); ++it)
+#define forall(it, v) for(auto it=v.begin(); it!=v.end(); ++it)
 #define sz(c) ((int)c.size())
 #define zero(v) memset(v, 0, sizeof(v))
 #define pb push_back
