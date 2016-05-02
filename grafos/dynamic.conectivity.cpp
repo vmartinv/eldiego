1e935b39dfb8508c64d5d6a4f500ccc3c0afe35a
Oia Poli <politecnico.oia@gmail.com>
2016-05-01 22:31:03 -0300

    fixed, welltested and styled dynconectivity

--- grafos/dynamic.conectivity.cpp
+++ grafos/dynamic.conectivity.cpp
@@ -14,152 +14,92 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int> ii;
 
-struct UnionFind{
-	vector<int> f;//the array contains the parent of each node
-	vector<pair<int&,int>> m;
-	void init(int n){m.clear();f.clear(); f.insert(f.begin(), n, -1);}
-	int comp(int x){return (f[x]==-1?x:assign(f[x],comp(f[x])));}//O(1)
-	bool join(int i, int j) {
-		bool con=comp(i)==comp(j);
-		if(!con) assign(f[comp(i)],comp(j));
-		return con;
-	}
-	int assign(int& l, int r) { m.emplace_back(l,l); return l=r; }
-	int snapt() {return sz(m);}
-	void rollback(int s) { 
-		while (sz(m)>s) { auto lr = m.back(); lr.fst=lr.snd; m.pop_back(); } 
-	}} uf;
-
-#define MAXQ 100000
-struct DynamicConnectivity {
-	enum querytype { ADD, DEL, QUERY };
-    struct query { 
-		int type,u,v; 
-		query(int type, int u, int v) : type(type), u(u), v(v) {}
-	};
-    int match[MAXQ];
-    vector<query> q;
+struct UnionFind {
+    int n, comp;
+    vector<int> pre,si,c;
+    UnionFind(int n=0):n(n), comp(n), pre(n), si(n, 1) {
+        forn(i,n) pre[i] = i; }
+    int find(int u){return u==pre[u]?u:find(pre[u]);}
+    bool merge(int u, int v) {
+        if((u=find(u))==(v=find(v))) return false;        
+        if(si[u]<si[v]) swap(u, v);
+        si[u]+=si[v], pre[v]=u, comp--, c.pb(v);
+        return true;
+    }
+    int snap(){return sz(c);}
+    void rollback(int snap){
+        while(sz(c)>snap){
+            int v = c.back(); c.pop_back();
+            si[pre[v]] -= si[v], pre[v] = v, comp++;
+        }
+    }
+};
+enum {ADD,DEL,QUERY};
+struct Query {int type,u,v;};
+struct DynCon {
+    vector<Query> q;
     UnionFind dsu;
-    map<ii,int> last;
-    map<ii,int> cante;
-    vector<bool> res;
-    
-
-    void init(int n) {
-        dsu.init(n);
-        last.clear();
-        res.clear();
-        q.clear();
-    }
-
+    vector<int> match,res;
+    map<ii,int> last;//se puede no usar cuando hay identificador para cada arista (mejora poco)
+    DynCon(int n=0):dsu(n){}
     void add(int u, int v) {
-        if (u > v) swap(u,v); 
-        if (cante[mkp(u,v)]++) return;
-        last[mkp(u,v)] = sz(q);
-        match[sz(q)] = -1;
-        q.pb(query(ADD,u,v));
+        if(u>v) swap(u,v);
+        q.pb((Query){ADD, u, v}), match.pb(-1);
+        last[ii(u,v)] = sz(q)-1;
     }
-
     void remove(int u, int v) {
-        if (u > v) swap(u,v);
-        if (!cante[mkp(u,v)]) return;
-        if (--cante[mkp(u,v)]) return;
-        int l = last[ii(u,v)];
-        match[l] = sz(q);
-        match[sz(q)] = l;
-        q.pb(query(DEL,u,v));
+        if(u>v) swap(u,v);
+        q.pb((Query){DEL, u, v});
+        int prev = last[ii(u,v)];
+        match[prev] = sz(q)-1;
+        match.pb(prev);
     }
-
-    void request(int u, int v) {
-        match[sz(q)] = -1;
-        q.pb(query(QUERY,u,v));
-    }
-
+    void query() {//podria pasarle un puntero donde guardar la respuesta
+        q.pb((Query){QUERY, -1, -1}), match.pb(-1);}
     void process() {
         forn(i,sz(q)) if (q[i].type == ADD && match[i] == -1) match[i] = sz(q);
-        if (sz(q)) go(0,sz(q));
+        go(0,sz(q));
     }
-
     void go(int l, int r) {
-		
-        if (l+1 == r) {
-            if (q[l].type == QUERY) {
-				//~ dprint(l); dprint(r);
-				int snap = dsu.snapt();
-				res.pb(dsu.comp(q[l].u)==dsu.comp(q[l].v));
-				dsu.rollback(snap);
-			}
+        if(l+1==r){
+            if (q[l].type == QUERY)//Aquí responder la query usando el dsu!
+                res.pb(dsu.comp);//aqui query=cantidad de componentes conexas
             return;
         }
-        int snap, m = (l+r) / 2;
-        snap = dsu.snapt();
-        forr(i,m,r) if (match[i] != -1 && match[i] < l) dsu.join(q[i].u, q[i].v);
+        int s=dsu.snap(), m = (l+r) / 2;
+        forr(i,m,r) if(match[i]!=-1 && match[i]<l) dsu.merge(q[i].u, q[i].v);
         go(l,m);
-        dsu.rollback(snap);
-
-        snap = dsu.snapt();
-        forr(i,l,m) if (match[i] != -1 && match[i] >= r) dsu.join(q[i].u, q[i].v);
+        dsu.rollback(s);
+        s = dsu.snap();
+        forr(i,l,m) if(match[i]!=-1 && match[i]>=r) dsu.merge(q[i].u, q[i].v);
         go(m,r);
-        dsu.rollback(snap);
+        dsu.rollback(s);
     }
 }dc;
-	
 
-const int MAXN=100100;
-int n,m;
-multiset<int> G[MAXN];
+// Problema ejemplo: http://codeforces.com/gym/100551/problem/A
 
-void addEdge(int a, int b) {
-	G[a].insert(b);
-	G[b].insert(a);
-}
-void removeEdge(int a, int b) {
-	G[a].erase(G[a].find(b));
-	G[b].erase(G[b].find(a));
-}
-vector<int> nres;
-bool vis[MAXN];
-void dfs(int v) {
-	vis[v]=true; 
-	forall(it,G[v]) if (!vis[*it]) dfs(*it);
-}
-void request(int a, int b) {
-	zero(vis);
-	//~ cout << "asd:\n";
-	//~ forall(it,G[a]) cout << *it << " "; cout << endl;
-	//~ forall(it,G[b]) cout << *it << " "; cout << "a:\n";
-	dfs(a);
-	nres.pb(vis[b]);
-}
+int n,k;
 
 int main() {
-    freopen("in", "r", stdin);
+    //~ freopen("in", "r", stdin);
+    freopen("connect.in", "r", stdin);
+    freopen("connect.out", "w", stdout);
     ios::sync_with_stdio(0);
-    while(cin >> n >> m){
-		dc.init(n);
-		forn(i,m) { int a,b,t; cin >>t >> a >> b;
-			
-			if (t==0) dc.add(a,b);
-			else if (t==1) dc.remove(a,b);
-			else dc.request(a,b);
-			
-			if (t==0) addEdge(a,b);
-			else if (t==1) removeEdge(a,b);
-			else request(a,b);
-			
+    while(cin >> n >> k){
+		dc=DynCon(n);
+		forn(_,k) { string ord; cin >> ord;
+			if (ord=="?") {
+				dc.query();
+			} else if (ord=="+") { int a,b; cin>>a>>b; a--;b--;
+				dc.add(a,b);
+			} else if (ord=="-") { int a,b; cin>>a>>b; a--;b--;
+				dc.remove(a,b);
+			} else assert(false);
 		}
-		
-		dc.process();
-		dprint(sz(dc.res));
-		dprint(sz(nres));
-		assert(sz(dc.res)==sz(nres));
-		forn(i,sz(dc.res)) {
-			cout << dc.res[i] << " " <<nres[i] << endl;
-			 assert(dc.res[i]==nres[i]);
-		 }
-		 cout << endl;
-		nres.clear();
-		forn(i,n) G[i].clear();
+        if(!k) continue;//k==0 WTF
+        dc.process();
+        forn(i,sz(dc.res)) cout << dc.res[i] << endl;
     }
     return 0;
 }
