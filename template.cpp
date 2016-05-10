085de84a6c8d6a6945e93f6b85291f9c220fc729
mariano22 <marianocrosetti1993@gmail.com>
2016-05-10 19:33:26 -0300

    redoredered template

--- template.cpp
+++ template.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define dprint(v) cout << #v"=" << v << endl //;)
 #define forr(i,a,b) for(int i=(a); i<(b); i++)
 #define forn(i,n) forr(i,0,n)
-#define dforn(i,n) for(int i=n-1; i>=0; i--)
-#define forall(it,v) for(auto it=v.begin();it!=v.end();++it)
 #define sz(c) ((int)c.size())
 #define zero(v) memset(v, 0, sizeof(v))
+#define forall(it,v) for(auto it=v.begin();it!=v.end();++it)
 #define pb push_back
 #define fst first
 #define snd second
 typedef long long ll;
 typedef pair<int,int> ii;
+#define dforn(i,n) for(int i=n-1; i>=0; i--)
+#define dprint(v) cout << #v"=" << v << endl //;)
 
 const int MAXN=100100;
 int n;
