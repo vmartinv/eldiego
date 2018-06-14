23f2117be2bb04a451047c5fc83a6e65e506c80c
karupayun <pablozcanalla@gmail.com>
2018-06-14 13:10:02 -0300

    Update exp.mod.cpp

--- math/exp.mod.cpp
+++ math/exp.mod.cpp
@@ -1,5 +1,5 @@
 #define MOD 1000000007
-ll expmod (ll b, ll e, ll m){//O(log b)
+ll expmod (ll b, ll e, ll m){//O(log e)
 	if(!e) return 1;
 	ll q= expmod(b,e/2,m); q=(q*q)%m;
 	return e%2? (b * q)%m : q;
